b46e9c317a97dd4dcd486dcf1bd71ecc453bb8e0
aneeka-rao <142113453+aneeka-rao@users.noreply.github.com>
2023-10-06 17:14:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let dog={}
+console.log(dog)
+dog={
+    name: "Max",
+    legs: 4,
+    color: "brown",
+    age: 5,
+    bark: function(){
+        return "woof woof"
+    }
+}
+console.log(dog.name)
+console.log(dog.legs)
+console.log(dog.color)
+console.log(dog.age)
+console.log(dog.bark())
+const users = {
+    Alex: {
+      email: "alex@alex.com",
+      skills: ["HTML", "CSS", "JavaScript"],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: "asab@asab.com",
+      skills: ["HTML", "CSS", "JavaScript", "Redux", "MongoDB", "Express", "React", "Node"],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: "daniel@daniel.com",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: "daniel@alex.com",
+      skills: ["HTML", "CSS", "JavaScript", "Python"],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: "john@john.com",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: "thomas@thomas.com",
+      skills: ["HTML", "CSS", "JavaScript", "React"],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: "paul@paul.com",
+      skills: ["HTML", "CSS", "JavaScript", "MongoDB", "Express", "React", "Node"],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+  }
+  users.Aneeka ={
+    email: "aneeka.rao",
+    skills: ["code", "js", "python"],
+    age: 12,
+    isLoggedIn: true,
+    points: 10,
+  }
+  console.log(Object.keys(users))
